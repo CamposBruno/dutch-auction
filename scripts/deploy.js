d5a05b95496cb9a3a26bd2c79150e017c4176f07
Bruno Campos <bc@unicsoft.com>
2024-03-01 16:19:11 -0300

    chore: added disclaimer to deploy NFT contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,26 +4,35 @@ require("dotenv").config();
 async function main() {
   const STARTING_PRICE = 1e6;
   const DISCOUNT_RATE = 1n;
-  const NFT_ID = 505;
+  
+  let NFT_ADDRESS = process.env.NFT_ADDRESS;
+  let NFT_ID = process.env.NFT_ID;
+  
+  // In Order for this auction to work, you must provide a valid NFT contract address and 
+  // a NFT id owned by the seller. If you do not have it uncomment this section if you want
+  // to deploy a new NFT contract and mint an empty NFT in order to test the contract.
+  
+  // const [owner] = await ethers.getSigners();
+  // const NftContract = await ethers.getContractFactory("MockNFT");
+  // NFT = await NftContract.deploy(owner);
+  // await NFT.waitForDeployment();
 
-  const [owner] = await ethers.getSigners();
+  // NFT_ADDRESS = await NFT.getAddress();
+  // NFT_ID = 505;
 
-  const NftContract = await ethers.getContractFactory("MockNFT");
-  NFT = await NftContract.deploy(owner);
-  await NFT.waitForDeployment();
-
-  const NFT_ADDRESS = await NFT.getAddress();
-
-  console.log('NFT created ad address', NFT_ADDRESS);
-
-  await NFT.safeMint(owner, NFT_ID);
-  await NFT.approve(owner, NFT_ID);
-
+  // await NFT.safeMint(owner, NFT_ID);
+  // console.log(`NFT ${NFT_ID} created with address ${NFT_ADDRESS}`);
+  
   const dutchAunctionContract = await ethers.getContractFactory("DutchAuction");
   DutchAuction = await dutchAunctionContract.deploy(STARTING_PRICE, DISCOUNT_RATE, NFT_ADDRESS, NFT_ID);
   await DutchAuction.waitForDeployment();
 
-  console.log('DutchAuction Deployed to ', await DutchAuction.getAddress());
+  // If you uncommented the previous section, do not forget to uncomment this section as well.
+  
+  // const auctionAddress = await DutchAuction.getAddress();
+  // await NFT.approve(auctionAddress, NFT_ID);
+
+  // console.log('DutchAuction Deployed to ', await DutchAuction.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
